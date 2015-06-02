f2833f567032ebd453b1e20c9016a9c444b31f6d
성기평 <sungkipyung@gmail.com>
2015-06-02 18:52:53 +0900

    test code

--- PostFormSampleTests/PostFormSampleTests.m
+++ PostFormSampleTests/PostFormSampleTests.m
@@ -34,6 +34,7 @@
     // This is an example of a performance test case.
     [self measureBlock:^{
         // Put the code you want to measure the time of here.
+        NSLog(@"hello world");
     }];
 }
 
