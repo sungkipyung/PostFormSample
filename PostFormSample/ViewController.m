56409c43f04c45b821daebb984ad9df7cc663a60
성기평 <sungkipyung@gmail.com>
2015-06-05 18:17:52 +0900

    iOS display:none implements

--- PostFormSample/ViewController.m
+++ PostFormSample/ViewController.m
@@ -10,9 +10,15 @@
 
 @interface ViewController () <UITextViewDelegate>
 @property (weak, nonatomic) IBOutlet UIScrollView *containerScrollView;
-@property (weak, nonatomic) UITextView *textViewContent;
+
+@property (weak, nonatomic) UITextView *textView;
 @property (assign, nonatomic) CGFloat initialTextViewHeight;
-@property (weak, nonatomic) NSLayoutConstraint *constraintContainerHeight;
+@property (weak, nonatomic) NSLayoutConstraint *constraintTextViewHeight;
+
+@property (weak, nonatomic) UICollectionView *collectionView;
+@property (weak, nonatomic) NSLayoutConstraint *constraintCollectionViewHeight;
+
+@property (weak, nonatomic) UIScrollView *labelView;
 @end
 
 @implementation ViewController
@@ -20,40 +26,90 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    _initialTextViewHeight = 30.0f;
+    
     
     CGFloat screenWidth = [UIScreen mainScreen].bounds.size.width;
     CGFloat screenHeight = [UIScreen mainScreen].bounds.size.height;
     [self.containerScrollView setBackgroundColor:[UIColor redColor]];
     self.containerScrollView.contentSize = CGSizeMake(screenWidth, screenHeight*2);
     
+    // textView
     UITextView *textView = [[UITextView alloc] init];
     textView.translatesAutoresizingMaskIntoConstraints = NO;
     textView.delegate = self;
     textView.scrollEnabled = NO;
     textView.backgroundColor = [UIColor yellowColor];
     
+    
+    // collectionView
+    UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc] init];
+    layout.itemSize = CGSizeMake(100, 100);
+    UICollectionView *collectionView = [[UICollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:layout];
+    collectionView.backgroundColor = [UIColor blackColor];
+    collectionView.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    // labelView
+    UIScrollView *labelView = [[UIScrollView alloc] init];
+    labelView.translatesAutoresizingMaskIntoConstraints = NO;
+    labelView.backgroundColor = [UIColor greenColor];
+    
+    [self.containerScrollView addSubview:collectionView];self.collectionView = collectionView;
+    [self.containerScrollView addSubview:textView];self.textView = textView;
+    [self.containerScrollView addSubview:labelView];self.labelView = labelView;
+    
+    // layout
     CGFloat margin = 16;
-    NSDictionary *viewDic = @{@"textView":textView};
-    NSDictionary *metrics = @{@"textViewMargin":@(margin), @"textViewWidth":@(screenWidth - 2 * margin), @"textViewDefaultHeight":@30};
+    NSDictionary *viewDic = @{@"textView":textView, @"collectionView":collectionView, @"labelView":labelView};
+    NSDictionary *metrics = @{@"margin":@(margin), @"width":@(screenWidth - 2 * margin)};
     
-    CGFloat height = 30.0f;
-    [self.containerScrollView addSubview:textView];
-    self.textViewContent = textView;
+    _initialTextViewHeight = 30.0f;
+    NSLayoutConstraint *constraintTextViewHeight
+    = [NSLayoutConstraint constraintWithItem:textView attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1 constant:_initialTextViewHeight];
     
-    NSLayoutConstraint *constraintContainerHeight
-    = [NSLayoutConstraint constraintWithItem:textView attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1 constant:height];
-    
-    [self.textViewContent addConstraint:constraintContainerHeight];
-    self.constraintContainerHeight = constraintContainerHeight;
+    [self.textView addConstraint:constraintTextViewHeight];
+    self.constraintTextViewHeight = constraintTextViewHeight;
     
     [self.containerScrollView addConstraints:
      [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-16-[textView]" options:0 metrics:metrics views:viewDic]];
     [self.containerScrollView addConstraints:
-     [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-16-[textView(textViewWidth)]-16-|" options:0 metrics:metrics views:viewDic]];
+     [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-margin-[textView(width)]-margin-|" options:0 metrics:metrics views:viewDic]];
+    
+    NSLayoutConstraint *constraintCollectionViewHeight
+    = [NSLayoutConstraint constraintWithItem:collectionView attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1 constant:100];
+    [self.collectionView addConstraint:constraintCollectionViewHeight];
+    self.constraintCollectionViewHeight = constraintCollectionViewHeight;
+    
+    [self.containerScrollView addConstraints:
+     [NSLayoutConstraint constraintsWithVisualFormat:@"V:[textView]-0-[collectionView]" options:0 metrics:metrics views:viewDic]];
+    [self.containerScrollView addConstraints:
+     [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-margin-[collectionView(width)]-margin-|" options:0 metrics:metrics views:viewDic]];
+    
+//    NSLayoutConstraint *constraintLabelViewHeight
+//    = [NSLayoutConstraint constraintWithItem:labelView attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1 constant:14];
+//    [self.collectionView addConstraint:constraintLabelViewHeight];
+    
+    [self.containerScrollView addConstraints:
+     [NSLayoutConstraint constraintsWithVisualFormat:@"V:[collectionView]-0-[labelView(14)]" options:0 metrics:metrics views:viewDic]];
+    [self.containerScrollView addConstraints:
+     [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-margin-[labelView(width)]-margin-|" options:0 metrics:metrics views:viewDic]];
+    
+    [self p_hideCollectionView];
 }
 
--(IBAction)touchAppendButton:(id)sender
+- (void)p_hideCollectionView
+{
+    self.constraintCollectionViewHeight.constant = 0.1;
+    self.collectionView.hidden = YES;
+}
+
+- (void)p_showCollectionView
+{
+    self.constraintCollectionViewHeight.constant = 100;
+    self.collectionView.hidden = NO;
+}
+
+
+- (IBAction)touchAppendButton:(id)sender
 {
     UIView *upperView = self.containerScrollView.subviews.firstObject;
     
@@ -91,12 +147,12 @@
         return YES;
     }
     if ([text isEqualToString:@"\n"]) {
-        self.textViewContent.text = [self.textViewContent.text stringByAppendingString:text];
+        self.textView.text = [self.textView.text stringByAppendingString:text];
         [self p_resizeTextView];
         NSLog(@"enter deteced");
         return NO;
     } else {
-        self.textViewContent.text = [self.textViewContent.text stringByAppendingString:text];
+        self.textView.text = [self.textView.text stringByAppendingString:text];
         [self p_resizeTextView];
         return NO;
     }
@@ -106,22 +162,22 @@
 {
     [self p_resizeTextView];
     NSLog(@"didChanged");
-    [self scrollToCaretInTextView:self.textViewContent animated:NO];
+    [self scrollToCaretInTextView:self.textView animated:NO];
 }
 
 - (void)p_resizeTextView
 {
     CGFloat height;
-    if (self.textViewContent.text.length == 0) {
+    if (self.textView.text.length == 0) {
         height = self.initialTextViewHeight;
     } else {
         const CGFloat mainScreenWidth = [UIScreen mainScreen].bounds.size.width;
-        [self glw_sizeThatFitsByFixedWidth:mainScreenWidth - 32 withTextView:self.textViewContent];
-        height = self.textViewContent.frame.size.height;
+        [self glw_sizeThatFitsByFixedWidth:mainScreenWidth - 32 withTextView:self.textView];
+        height = self.textView.frame.size.height;
     }
-    self.textViewContent.frame = CGRectMake(self.textViewContent.frame.origin.x, self.textViewContent.frame.origin.y, self.textViewContent.frame.size.width, height);
+    self.textView.frame = CGRectMake(self.textView.frame.origin.x, self.textView.frame.origin.y, self.textView.frame.size.width, height);
     self.containerScrollView.contentSize = CGSizeMake(self.containerScrollView.contentSize.width, height);
-    [self.constraintContainerHeight setConstant:height];
+    [self.constraintTextViewHeight setConstant:height];
     
 }
 
@@ -129,6 +185,13 @@
     CGRect rect = [textView caretRectForPosition:textView.selectedTextRange.end];
     rect.size.height += textView.textContainerInset.bottom;
     [textView scrollRectToVisible:rect animated:animated];
+}
+- (IBAction)toggleCollectionView:(id)sender {
+    if (self.collectionView.hidden) {
+        [self p_showCollectionView];
+    } else {
+        [self p_hideCollectionView];
+    }
 }
 
 - (void)glw_sizeThatFitsByFixedWidth:(CGFloat)fixedWidth withTextView:(UITextView *)textView
